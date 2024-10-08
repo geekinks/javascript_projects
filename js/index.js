3ece70d98aaf91b48021866adf7cfc37356471d8
Adamu Muhammad Muhammad <muhdadamu0008@gmail.com>
2024-10-08 15:07:37 +0100

    feat: js class

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-alert("welcome to the world of JavaScript");
+var pk = document.getElementsByTagName('p')[0];
+var btn = document.getElementById('reg');
+
+btn.addEventListener('click', function() {
+    let conetnt = [
+        "Hello, World!",
+        "welcome to JavaScript",
+        "I am a front-end developer",
+        "I love JavaScript",
+        "I love coding",
+        "I am a web developer",
+        "I am a programmer",
+        "I am a designer",
+    ]
+
+  pk.innerHTML = conetnt[Math.floor(Math.random() * conetnt.length)];
+});
