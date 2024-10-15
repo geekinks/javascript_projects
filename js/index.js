536cafa60757e88015bd269cd33c667157147308
Adamu Muhammad Muhammad <muhdadamu0008@gmail.com>
2024-10-15 10:52:13 +0100

    learn: function and fn type

--- js/index.js
+++ js/index.js
@@ -1,17 +1,38 @@
-var pk = document.getElementsByTagName('p')[0];
+var pk = document.getElementsByTagName('p')[2];
 var btn = document.getElementById('reg');
 
-btn.addEventListener('click', function() {
-    let conetnt = [
-        "Hello, World!",
-        "welcome to JavaScript",
-        "I am a front-end developer",
-        "I love JavaScript",
-        "I love coding",
-        "I am a web developer",
-        "I am a programmer",
-        "I am a designer",
-    ]
+const clickHandler = () => {
+  let conetnt = [
+      "Hello, World!",
+      "welcome to JavaScript",
+      "I am a front-end developer",
+      "I love JavaScript",
+      "I love coding",
+      "I am a web developer",
+      "I am a programmer",
+      "I am a designer",
+  ]
+  
+  let random = Math.floor(Math.random() * conetnt.length);
 
-  pk.innerHTML = conetnt[Math.floor(Math.random() * conetnt.length)];
-});
+
+pk.innerHTML = conetnt[random];
+};
+
+// clickHandler();
+
+btn.addEventListener('click', clickHandler);
+
+let username = prompt("What is your name?");
+
+const greeet = function(name){
+  return "Hello, " + name;
+}
+
+console.log(greet(username));
+
+// window.alert(greet(username));
+
+setTimeout(function() {
+  console.log('welcome to js world')
+}, 1000)
